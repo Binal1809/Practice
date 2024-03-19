44ac33720de6a53c8501611aacccbacc65f6e2be
Binal1809 <shah.binal1892@gmail.com>
2024-03-19 02:01:20 -0700

    webelements basics

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -12,6 +12,9 @@ describe('my first test', function()
                 cy.get('.search-keyword').type('ca')
                 // implicit wait
                 cy.wait(2000)
+                // aliasing concept to give name to webelements 
+                cy.get('.products').as('productLocator')
+                cy.get('@productLocator').find('.product').should('have.length',4)
                 // assertion on number of elements
                 cy.get('.product').should('have.length',5)
                 // assertion on visible number of elements
@@ -21,7 +24,31 @@ describe('my first test', function()
                 //cy.get('nth:child(3) > .product-action > button').click()
                 cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
                 
-                
+                //cypress is asynchronus
+                cy.get('.products').find('.product').each(($e1,index,$list) =>
+                {
+                    const name = $e1.find('h4.product-name').text()
+                    if(name.includes('Cashews'))
+                        $e1.find('div.product-action button').click()
+                }
+                )
+
+                // handling promise
+                /* text() is jquery method, so it works on resolved promise, 
+                   will not work on not resolve promise use then method resolve the promise and run*/
+
+                cy.get('.brand').then(function(logoelement)
+                {
+                    cy.log(logoelement.text())
+                })
+
+                // cypress commands like cy.log woild be sequential, where as console.log('sf') is non cypress will be asynchronus
+                // to get console.log() as syquential we need to add then()
+
+                // assertion , should from chai library has all syncs 
+                cy.get('.brand').should('have.text','GREENKART')
+
+
 
                 
             })
